8cd8b9fb2048155b774631c0c0d2e9943a2dc9d4
john <hirokiht@gmail.com>
2015-07-21 18:23:47 +0800

    Refactored to become more generic Data Logger

--- app/src/main/java/tw/edu/ncku/letsdance/SensorDataLoggerFragment.java
+++ app/src/main/java/tw/edu/ncku/letsdance/SensorDataLoggerFragment.java
@@ -19,20 +19,24 @@ import java.util.Arrays;
  * create an instance of this fragment.
  */
 public class SensorDataLoggerFragment extends Fragment {
-    private String mac;
+    private String prefix;
     private SimpleArrayMap<String,ArrayList<float[]>> data = new SimpleArrayMap<>();
+
+    public static SensorDataLoggerFragment newInstance() {
+        return new SensorDataLoggerFragment();
+    }
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param mac Mac address of the Bluetooth Device to monitor.
+     * @param prefix Prefix for filename to be saved.
      * @return A new instance of fragment SensorDataLoggerFragment.
      */
-    public static SensorDataLoggerFragment newInstance(String mac) {
+    public static SensorDataLoggerFragment newInstance(String prefix) {
         SensorDataLoggerFragment fragment = new SensorDataLoggerFragment();
         Bundle args = new Bundle();
-        args.putString("mac", mac);
+        args.putString("prefix", prefix);
         fragment.setArguments(args);
         return fragment;
     }
@@ -44,9 +48,7 @@ public class SensorDataLoggerFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() == null)
-            return;
-        mac = getArguments().getString("mac");
+        prefix = getArguments() != null? getArguments().getString("prefix") : "";
     }
 
     public void logData(String key, float[] value){
@@ -60,7 +62,7 @@ public class SensorDataLoggerFragment extends Fragment {
             throw new IOException("ext storage not mounted! State: "+Environment.getExternalStorageState());
         final File dir = getActivity().getExternalFilesDir(null);
         for(int i = 0 ; i < data.size() ; i++){
-            File fp = new File(dir,data.keyAt(i)+"-"+mac+".csv");
+            File fp = new File(dir,prefix+"-"+data.keyAt(i)+".csv");
             BufferedWriter writer = new BufferedWriter(new FileWriter(fp));
             for(float[] d : data.valueAt(i)) {
                 writer.write(Arrays.toString(d));
